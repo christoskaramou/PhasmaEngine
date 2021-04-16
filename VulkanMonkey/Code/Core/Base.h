675d6f230bb38f8e388979f9f9db2b8d566a3ef1
Christos Karamoustos <christos.karamoustos@pixeltoys.com>
2021-04-16 23:11:36 +0300

    Significantly reduced the copies of make_ref

--- VulkanMonkey/Code/Core/Base.h
+++ VulkanMonkey/Code/Core/Base.h
@@ -1,26 +1,26 @@
 #pragma once
 #include <memory>
-#include <vector>
 
 namespace vm
 {
 	template<class T>
 	using Ref = std::shared_ptr<T>;
 
-	template <class T>
-	Ref<T> make_ref(const T& obj)
-	{
-		return std::make_shared<T>(obj);
-	}
+    template <class T>
+    Ref<T> make_ref(T& obj)
+    {
+        return std::make_shared<T>(obj);
+    }
 
 	template <class T>
-	std::vector<Ref<T>> make_ref_vec(const std::vector<T>& vec)
-	{
-		std::vector ref_vec(vec.size());
+    Ref<T> make_ref(const T& obj)
+    {
+        return std::make_shared<T>(obj);
+    }
 
-		for (int i = 0; i < vec.size(); i++)
-			ref_vec[i] = std::make_shared<T>(vec[i]);
-
-		return ref_vec;
-	}
+    template <class T>
+    Ref<T> make_ref(T&& obj)
+    {
+        return std::make_shared<T>(std::forward<T>(obj));
+    }
 }
