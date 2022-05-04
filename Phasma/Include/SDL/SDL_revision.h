61e676f0f3a9175bef9662b4bda3477962968b8b
christoskaramou <christoskar@live.com>
2022-05-04 01:16:07 +0100

    Vulkan update (1.3.211.0)

--- Phasma/Include/SDL/SDL_revision.h
+++ Phasma/Include/SDL/SDL_revision.h
@@ -1,2 +1,6 @@
-#define SDL_REVISION "https://github.com/libsdl-org/SDL.git@b424665e0899769b200231ba943353a5fee1b6b6"
+#define SDL_REVISION "git@github.com:libsdl-org/SDL.git@b424665e0899769b200231ba943353a5fee1b6b6"
 #define SDL_REVISION_NUMBER 0
+
+#ifndef SDL_REVISION
+#define SDL_REVISION ""
+#endif
