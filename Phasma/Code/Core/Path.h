4b9474aff9752366147a7c5f13202cf7eadd8a10
Christos Karamoustos <chriskarpap@gmail.com>
2024-11-01 19:57:59 +0200

    Add support for Unix-like systems in Path class initialization

--- Phasma/Code/Core/Path.h
+++ Phasma/Code/Core/Path.h
@@ -1,5 +1,11 @@
 #pragma once
 
+#ifdef WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
+
 namespace pe
 {
     class Path
@@ -19,6 +25,18 @@ namespace pe
                 char str[MAX_PATH];
                 GetModuleFileNameA(nullptr, str, MAX_PATH);
                 Executable = std::filesystem::path(str).remove_filename().string();
+#else
+                char str[PATH_MAX];
+                ssize_t length = readlink("/proc/self/exe", str, sizeof(str) - 1);
+                if (length < 0)
+                {
+                    Executable = std::filesystem::current_path().string();
+                }
+                else
+                {
+                    str[length] = '\0'; // Null-terminate the string
+                    Executable = std::filesystem::path(str).remove_filename().string();
+                }
 #endif
 
                 if (!std::filesystem::exists(Executable))
