c85aaac6dc58ab287b1622fece5f9701f19cfa52
Christos Karamoustos <chriskarpap@gmail.com>
2025-10-07 21:34:47 +0300

    Refactor ThreadPool constructor and Enqueue method

--- PhasmaCore/Code/Base/ThreadPool.cpp
+++ PhasmaCore/Code/Base/ThreadPool.cpp
@@ -2,52 +2,54 @@
 
 namespace pe
 {
-    // externs
-    ThreadPool e_ThreadPool(std::thread::hardware_concurrency());        // general ThreadPool
-    ThreadPool e_Update_ThreadPool(std::thread::hardware_concurrency()); // Update ThreadPool
-    ThreadPool e_Render_ThreadPool(std::thread::hardware_concurrency()); // Render ThreadPool
-    ThreadPool e_FW_ThreadPool(1);                                       // FileWatcher ThreadPool
-    ThreadPool e_GUI_ThreadPool(std::thread::hardware_concurrency());    // GUI ThreadPool
-
-    // the constructor just launches some amount of workers
-    inline ThreadPool::ThreadPool(size_t threads) : m_stop{false}
+    static size_t ClampThreads(size_t n)
     {
-        m_workers.reserve(threads);
+        return std::max<size_t>(1, n);
+    }
 
-        for (size_t i = 0; i < threads; ++i)
+    ThreadPool::ThreadPool(size_t threads)
+    {
+        const size_t count = ClampThreads(threads);
+        m_workers.reserve(count);
+        for (size_t i = 0; i < count; ++i)
         {
-            m_workers.emplace_back(
-                [this]
+            m_workers.emplace_back([this]
+                                   {
+            for (;;) {
+                std::optional<std::function<void()>> task;
                 {
-                    for (;;)
-                    {
-                        std::optional<std::function<void()>> task;
-                        {
-                            std::unique_lock<std::mutex> lock(m_queue_mutex);
-                            m_condition.wait(lock, [this]
-                                             { return m_stop || !m_tasks.empty(); });
-
-                            if (m_stop && m_tasks.empty())
-                                return;
-
-                            task = std::move(m_tasks.front());
-                            m_tasks.pop_front();
-                        }
-                        (*task)();
-                    }
-                });
+                    std::unique_lock<std::mutex> lock(m_queue_mutex);
+                    m_condition.wait(lock, [this] {
+                        return m_stop || !m_tasks.empty();
+                    });
+                    if (m_stop && m_tasks.empty())
+                        return;
+                    task = std::move(m_tasks.front());
+                    m_tasks.pop_front();
+                }
+                (*task)();
+            } });
         }
     }
 
-    // the destructor joins all threads
-    inline ThreadPool::~ThreadPool()
+    ThreadPool::~ThreadPool()
     {
         {
             std::unique_lock<std::mutex> lock(m_queue_mutex);
             m_stop = true;
         }
         m_condition.notify_all();
-        for (std::thread &worker : m_workers)
-            worker.join();
+        for (std::thread &t : m_workers)
+        {
+            if (t.joinable())
+                t.join();
+        }
     }
-}
+
+    static const size_t N = ClampThreads(std::thread::hardware_concurrency());
+    ThreadPool e_ThreadPool(N);
+    ThreadPool e_Update_ThreadPool(N);
+    ThreadPool e_Render_ThreadPool(N);
+    ThreadPool e_FW_ThreadPool(1);
+    ThreadPool e_GUI_ThreadPool(N);
+} // namespace pe
