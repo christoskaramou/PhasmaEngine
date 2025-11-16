f1591034984ccc498a46fb6d17d30e9b4205823d
Christos Karamoustos <chriskarpap@gmail.com>
2025-11-17 00:55:07 +0200

    Remove redundant methods in Surface

--- PhasmaCore/Code/API/Surface.cpp
+++ PhasmaCore/Code/API/Surface.cpp
@@ -17,27 +17,15 @@ namespace pe
         int w, h;
         SDL_Vulkan_GetDrawableSize(window, &w, &h);
         m_actualExtent = Rect2Du{0, 0, static_cast<uint32_t>(w), static_cast<uint32_t>(h)};
-    }
 
-    Surface::~Surface()
-    {
-        if (m_apiHandle)
-            RHII.GetInstance().destroySurfaceKHR(m_apiHandle);
-    }
-
-    void Surface::CheckTransfer()
-    {
+        // Check transfer support
         auto capabilities = RHII.GetGpu().getSurfaceCapabilitiesKHR(m_apiHandle);
-
         // Ensure blit operations
         vk::ImageUsageFlags flags = vk::ImageUsageFlagBits::eTransferSrc | vk::ImageUsageFlagBits::eTransferDst | vk::ImageUsageFlagBits::eSampled;
         PE_ERROR_IF(!(capabilities.supportedUsageFlags & flags), "Surface doesnt support nessesary operations");
-    }
 
-    void Surface::FindFormat()
-    {
+        // Find format
         auto formats = RHII.GetGpu().getSurfaceFormatsKHR(m_apiHandle);
-
         m_format = vk::Format::eUndefined;
         for (const auto &format : formats)
         {
@@ -49,8 +37,15 @@ namespace pe
                 break;
             }
         }
-
         PE_ERROR_IF(m_format == vk::Format::eUndefined, "Surface format not found");
+
+        SetPresentMode(vk::PresentModeKHR::eFifo);
+    }
+
+    Surface::~Surface()
+    {
+        if (m_apiHandle)
+            RHII.GetInstance().destroySurfaceKHR(m_apiHandle);
     }
 
     std::vector<vk::PresentModeKHR> Surface::GetSupportedPresentModes() const
@@ -58,34 +53,15 @@ namespace pe
         return RHII.GetGpu().getSurfacePresentModesKHR(m_apiHandle);
     }
 
-    bool Surface::SupportsPresentMode(vk::PresentModeKHR mode)
+    void Surface::SetPresentMode(vk::PresentModeKHR preferredMode)
     {
         const auto &presentModes = GetSupportedPresentModes();
         for (const auto &presentMode : presentModes)
         {
-            if (presentMode == mode)
-                return true;
+            if (presentMode == preferredMode)
+                return void(m_presentMode = presentMode);
         }
 
-        return false;
-    }
-
-    void Surface::SetPresentMode(vk::PresentModeKHR preferredMode)
-    {
-        if (SupportsPresentMode(preferredMode))
-        {
-            m_presentMode = preferredMode;
-        }
-        else
-        {
-            m_presentMode = vk::PresentModeKHR::eFifo;
-        }
-    }
-
-    void Surface::FindProperties()
-    {
-        CheckTransfer();
-        FindFormat();
-        SetPresentMode(vk::PresentModeKHR::eFifo);
+        m_presentMode = vk::PresentModeKHR::eFifo;
     }
 }
--- PhasmaCore/Code/API/Surface.h
+++ PhasmaCore/Code/API/Surface.h
@@ -10,10 +10,7 @@ namespace pe
         Surface(SDL_Window *window);
         ~Surface();
 
-        void CheckTransfer();
-        void FindFormat();
         void SetPresentMode(vk::PresentModeKHR preferredMode);
-        void FindProperties();
         const Rect2Du &GetActualExtent() const { return m_actualExtent; }
         void SetActualExtent(const Rect2Du &extent) { m_actualExtent = extent; }
         vk::Format GetFormat() const { return m_format; }
@@ -22,8 +19,6 @@ namespace pe
         std::vector<vk::PresentModeKHR> GetSupportedPresentModes() const;
 
 private:
-        bool SupportsPresentMode(vk::PresentModeKHR mode);
-
         Rect2Du m_actualExtent;
         vk::Format m_format;
         vk::ColorSpaceKHR m_colorSpace;
