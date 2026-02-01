745f41e3fc852ec7db0d7c953336cdfdbba091bb
Christos Karamoustos <chriskarpap@gmail.com>
2026-02-01 21:04:15 +0200

    add Editor styles and Fonts

---
+++ PhasmaEditor/Code/GUI/IconsFontAwesome.h
@@ -0,0 +1,24 @@
+#pragma once
+
+// FontAwesome 6 Free - Icon definitions
+// Icons from Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com
+// License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
+
+// Icon font configuration
+#define ICON_MIN_FA 0xf000
+#define ICON_MAX_FA 0xf8ff
+#define FONT_ICON_FILE_NAME_FAS "fa-solid-900.ttf"
+
+// Icons used in Hierarchy
+#define ICON_FA_CUBE "\xef\x86\xb2"           // f1b2 - cube (for models)
+#define ICON_FA_CUBES "\xef\x86\xb3"          // f1b3 - cubes (for model groups)
+#define ICON_FA_CIRCLE "\xef\x84\x91"         // f111 - circle (for generic nodes)
+#define ICON_FA_CIRCLE_DOT "\xef\x86\x92"     // f192 - circle-dot (for mesh nodes)
+#define ICON_FA_VECTOR_SQUARE "\xef\x97\x8b"  // f5cb - vector-square (for nodes)
+#define ICON_FA_VIDEO "\xef\x80\xbd"          // f03d - video (for cameras)
+#define ICON_FA_LIGHTBULB "\xef\x83\xab"      // f0eb - lightbulb (for lights)
+#define ICON_FA_FOLDER "\xef\x81\xbb"         // f07b - folder (for empty parent nodes)
+#define ICON_FA_FILE "\xef\x85\x9b"           // f15b - file (for empty leaf nodes)
+#define ICON_FA_SITEMAP "\xef\x83\xa8"        // f0e8 - sitemap (for hierarchies)
+#define ICON_FA_SHAPES "\xef\x98\x9f"         // f61f - shapes (for meshes)
+#define ICON_FA_DRAW_POLYGON "\xef\x97\xae"   // f5ee - draw-polygon (for mesh geometry)
